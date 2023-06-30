173e226c8074e789ee62d6c2cd996cd6c00b26ab
John Gettings <johngettingsai@gmail.com>
2023-06-30 10:07:22 -0700

    Added simple webcam display

--- YOLO_NAS/YOLO_NAS/YOLO_NAS.cpp
+++ YOLO_NAS/YOLO_NAS/YOLO_NAS.cpp
@@ -1,20 +1,66 @@
-// YOLO_NAS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+//#include <sstream>
+#include <opencv2/opencv.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+        cv::namedWindow("Display Webcam", cv::WINDOW_AUTOSIZE);
+        cv::VideoCapture cap;
+        cap.open(0);
+
+        if (!cap.isOpened()) {
+            std::cerr << "Couldn't open camera" << std::endl;
+            return -1;
+        }
+
+        cv::Mat frame;
+
+        for (;;) {
+            cap >> frame;
+            if (frame.empty()) break;
+
+            // Mirroring image
+            cv::Mat frame_flipped;
+            cv::flip(frame, frame_flipped, 1);
+
+
+            // Display
+            cv::imshow("Display Webcam", frame_flipped);
+
+            // Display new frame every 20 ms (50 fps)
+            if ((char)cv::waitKey(20) >= 0) break;
+        }
+        return 0;
+
+
+    //int width = frame.size().width;
+    //int height = frame.size().height;
+
+
+
+
+
+    //From internet, not tested
+
+    //cv::Mat imageBGR = cv::imread(imageFilepath, cv::ImreadModes::IMREAD_COLOR);
+    //cv::Mat resizedImageBGR, resizedImageRGB, resizedImage, preprocessedImage;
+    //cv::resize(imageBGR, resizedImageBGR,
+    //    cv::Size(inputDims.at(2), inputDims.at(3)),
+    //    cv::InterpolationFlags::INTER_CUBIC);
+    //cv::cvtColor(resizedImageBGR, resizedImageRGB,
+    //    cv::ColorConversionCodes::COLOR_BGR2RGB);
+    //resizedImageRGB.convertTo(resizedImage, CV_32F, 1.0 / 255);
+
+    //cv::Mat channels[3];
+    //cv::split(resizedImage, channels);
+    //// Normalization per channel
+    //// Normalization parameters obtained from
+    //// https://github.com/onnx/models/tree/master/vision/classification/squeezenet
+    //channels[0] = (channels[0] - 0.485) / 0.229;
+    //channels[1] = (channels[1] - 0.456) / 0.224;
+    //channels[2] = (channels[2] - 0.406) / 0.225;
+    //cv::merge(channels, 3, resizedImage);
+    //// HWC to CHW
+    //cv::dnn::blobFromImage(resizedImage, preprocessedImage);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
